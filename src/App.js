5d4b17a09a5f0c9c7100dcd58f7e80d8fbea6627
Dima <uloga_d@hotmail.com>
2024-09-14 19:43:40 +0300

    dz1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const txt = "Текст передан через props";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Передача текста</h1>
+      <Message content = {txt}/>
     </div>
   );
+}
+
+function Message({content}) {
+  return (
+    <p className='paragr'>{content}</p>
+  )
 }
 
 export default App;
